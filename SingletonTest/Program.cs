57ef9fa52d04ced1e22dd83ea8974b39dd3b317c
Sofia_L <2964162259@qq.com>
2020-12-14 12:12:11 +0800

    单例模式

---
+++ SingletonTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    class SingletonProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("饿汉式单例模式");
+            Console.WriteLine();
+            //通过new实例化会报错
+            Singleton g = new Singleton();
+
+            //只能通过getInstance得到对象
+            Singleton g1 = Singleton.getInstance();
+            Console.ReadKey();
+
+            Console.WriteLine("懒汉式单例模式");
+            Console.WriteLine();
+            Console.WriteLine("单线程环境");
+            for(int i = 0; i < 10; i++)
+            {
+                LazySingleton singleton = LazySingleton.getInstance();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonTest/Singleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    /// <summary>
+    /// 饿汉式单例模式
+    /// </summary>
+    public class Singleton
+    {
+        //私有化构造方法，使得该类无法在外部通过new进行实例化
+        private void Single()
+        {
+            Console.WriteLine("这里是私有化构造方法");//不打印
+        }
+
+        //准备一个类属性，指向一个实例化对象，因为是类属性，所以只有一个
+        private static Singleton instance = new Singleton();
+
+        //public static方法，提供给调用者获取下面代码的对象
+        public static Singleton getInstance()
+        {
+            Console.WriteLine("通过一个static方法,调用类属性");
+            return instance;
+        }
+    }
+    /// <summary>
+    /// 懒汉式单例模式
+    /// </summary>
+    public class LazySingleton
+    {
+        //定义对象赋值为null
+        private static LazySingleton singleton = null;
+
+        //定义私有构造方法
+        private LazySingleton()
+        {
+            Console.WriteLine("创建了懒汉式单例模式对象");
+        }
+
+        //返回对象
+        public static LazySingleton getInstance()
+        {
+            // 判断如果对象不为null，就不用进入同步块，这样可以提高性能
+            if(singleton == null)
+            {
+                //判断对象为null，才创建对象
+                singleton = new LazySingleton();
+            }
+            return singleton;
+        }
+    }
+
+
+}
